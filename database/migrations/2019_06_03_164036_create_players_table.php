b759a016266f4667e3e9d044b386f996a0aab0a2
Milos <komaracdh@gmail.com>
2019-06-03 18:49:57 +0200

    v2

---
+++ database/migrations/2019_06_03_164036_create_players_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('first_name');
+            $table->text('last_name');
+            $table->text('email');
+            $table->unsignedBigInteger('team_id');
+            $table->foreign('team_id')
+            ->references('id')
+            ->on('teams')
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
